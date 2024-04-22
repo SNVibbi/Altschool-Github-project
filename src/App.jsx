91eae831e4c92578d9cc1e0daf9c88dc28476bb5
Simeon Vibbi <svibbinyakeh@gmail.com>
2024-04-22 02:11:29 -0700

    Fix deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import Profile from './components/Profile';
 import './App.css';
