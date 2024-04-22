28ddec29543806bb4e932474f6e376aee703c115
Simeon Vibbi <svibbinyakeh@gmail.com>
2024-04-22 02:21:59 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:/'Alschool GitHub'/,
+  base:'/AlschoolGitHub/',
   plugins: [react()]
 })
